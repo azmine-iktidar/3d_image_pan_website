dd9d3c58ccc4eaaf4e313242b17468cb38856d0b
Azmine  Iktidar <iktidar2978@gmail.com>
2024-12-23 23:05:54 +0600

    enhance Footer component styling with backdrop blur effect

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,7 +1,7 @@
 // components/Footer.js
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 w-full border-t-2 bg-gray-800/10 p-4 text-white lg:absolute">
+    <footer className="absolute bottom-0 w-full border-t-2 bg-gray-800/10 bg-opacity-[0] bg-clip-padding p-4 text-white backdrop-blur-sm backdrop-filter lg:absolute">
       <p className="text-center">© 2025 Sinataav</p>
     </footer>
   );
